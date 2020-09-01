fb42926c0ba958fb3b14003b0ae66b4f818a06e5
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2020-09-01 20:37:26 +0900

    Add files via upload

--- inflearn_12.java
+++ inflearn_12.java
@@ -4,28 +4,46 @@ public class inflearn_12 {
 	
 	public static void main(String[] args) {
 		
-		inflearn_10 myCar1 = new inflearn_10();
-		myCar1.color = "red";
-		myCar1.gear = "auto";
-		myCar1.price = 100;
+		inflearn_10 myCar1 = new inflearn_10(); // Grandeur constructor, 변수(myCar1) 선언, inflearn_10클래스에 있는 생성자
+		myCar1.color = "red"; // myCar1.color = red, inflearn_10클래스에서 선언한 변수(color)에 지정
+		myCar1.gear = "auto"; // myCar1.gear = auto, inflearn_10클래스에서 선언한 변수(gear)에 지정
+		myCar1.price = 100; // myCar1.price = 100, inflearn_10클래스에서 선언한 변수(price)에 지정
 		
-		myCar1.run();
-		myCar1.stop();
-		myCar1.info();
+		myCar1.run(); // --run--, inflearn_10클래스에 있는 메서드(public void run(){})를 실행한다.
+		myCar1.stop(); // --stop--, inflearn_10클래스에 있는 메서드(public void stop(){})를 실행한다.
+		myCar1.info(); /* --info--  inflearn_10클래스에 있는 메서드(public void stop(){})를 실행한다.
+		 				  color:red
+		 				  gear:auto
+		 				  price:100 */
 		
-		myCar1.color = "yellow";
-		myCar1.gear = "auto";
-		myCar1.price = 500;
+		myCar1.color = "yellow"; // myCar1.color = yellow
+		myCar1.gear = "auto"; // myCar1.gear = auto
+		myCar1.price = 500; // myCar1.price = 500
 		
-		myCar1.run();
-		myCar1.stop();
-		myCar1.info();
+		myCar1.run(); // --run--
+		myCar1.stop(); //--stop
+		myCar1.info(); /* --info--
+		 				  color:yellow
+		 				  gear:auto
+		 				  price:500 */
 		
-		inflearn_11 myBicycle = new inflearn_11("red",100);
-		myBicycle.info();
+		inflearn_11 myBicycle1 = new inflearn_11("red",100); // Bicycle constructor-2, 변수(myBicycle1) 선언, inflearn_11클래스에 있는 생성자(public inflearn(String c,int p){})
+		myBicycle1.info(); /* --info--
+		 					  color:red
+		 					  price:100 */
 		
-		myBicycle.color = "yellow";
-		myBicycle.info();
+		myBicycle1.color = "yellow"; // myBicycle1.color = yellow, inflearn_11클래스에서 선언한 변수(color)에 지정
+		myBicycle1.info(); /* --info--
+		 					  color:yellow
+		 					  price:100 */
+		
+		inflearn_11 myBicycle2 = new inflearn_11(); // Bicycle constructor-1, 변수(myBicycle2) 선언, inflearn_11클래스에 있는 생성자(public inflearn(){})생성자
+		
+		myBicycle2.color = "blue"; // myBicycle.color = blue, inflearn_11클래스에서 선언한 변수(color)에 지정
+		myBicycle2.price = 200; // mybicycle.price = 200, inflearn_11클래스에서 선언한 변수(price)에 지정
+		myBicycle2.info(); /* --info--
+		 					  color:blue
+		 					  price:200 */
 	}
 
 }
--- inflearn_13.java
+++ inflearn_13.java
@@ -3,10 +3,15 @@ package inflearn;
 public class inflearn_13 {
 	public static void main(String[] args) {
 		
-		inflearn_14 child1 = new inflearn_14();
-		child1.setInfo("hong gil dong", "M", 20);
+		inflearn_14 child1 = new inflearn_14(); // --ChildClass constructor--, inflearn_14에 있는 생성자
+		child1.setInfo("hong gil dong", "M", 20); /* child1.name = hong gil dong
+													 child1.gender = M
+													 child1.age = 20 */
 		
-		child1.getInfo();
+		child1.getInfo(); /* --getInfo() START--
+		 					 name:hong gil dong
+		 					 gender:M
+		 					 age:20 */
 	}
 
 }
