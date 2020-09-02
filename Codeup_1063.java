5a6bc80bbad4c1d203589ecefe09aa6e12d4c165
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2020-09-02 18:52:07 +0900

    Add files via upload

---
+++ Codeup_1063.java
@@ -0,0 +1,19 @@
+﻿package CodeUp;
+
+import java.util.Scanner;
+
+public class Codeup_1063 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); // sc = 입력될 값
+		int i = sc.nextInt(); // i = sc = 123
+		int j = sc.nextInt(); // j = sc = 456
+		int k = (i>j) ? i:j; // k = 456, (i>j) ? i:j = i>j가 true일 경우 i false일 경우 j
+		
+		System.out.println(k); // 456
+
+		sc.close();
+		}
+	}
+
---
+++ Codeup_1069.java
@@ -0,0 +1,33 @@
+﻿package CodeUp;
+
+import java.util.Scanner;
+
+public class Codeup_1069 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); // sc = 입력될 값 = A
+		String s = sc.next(); // s = sc = A
+		
+		switch(s) {
+		case "A": // s==A = true
+			System.out.println("best!!!"); // 조건 case가 true일 경우 출력한다.
+		break; // case가 실행됐을 경우 이 지점에서 switch의 실행을 멈춘다.
+		case "B": // s==B = false
+			System.out.println("good!!"); // 조건 case가 true일 경우 출력한다.
+		break; // case가 실행됐을 경우 이 지점에서 switch의 실행을 멈춘다.
+		case "C": // s==C = false
+			System.out.println("run!"); // 조건 case가 true일 경우 출력한다.
+		break; // case가 실행됐을 경우 이 지점에서 switch의 실행을 멈춘다.
+		case "D": // s==D = false
+			System.out.println("slowly~"); // 조건 case가 true일 경우 출력한다.
+		break; // case가 실행됐을 경우 이 지점에서 switch의 실행을 멈춘다.
+		default: // case들 모두에 해당 하지않을 경우 실행한다.
+			System.out.println("what?"); // default가 샐행 될 경우 출력한다.
+		break; // default가 실행됐을 경우 이 지점에서 switch의 실행을 멈춘다.
+		}
+
+		sc.close();
+		}
+	}
+
