baa54ef52bdda917579b2efc69ba69c91cc44eb5
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2020-09-13 16:29:46 +0900

    Add files via upload

--- inflearn/src/inflearn_19/main/Main.java
+++ inflearn/src/inflearn_19/main/Main.java
@@ -1,22 +1,47 @@
 package inflearn_19.main;
 
+import inflearn_19.employee.Employee;
 import inflearn_19.employee.EmployeeBank;
 
 public class Main {
 	public static void main(String[] args) {
 		EmployeeBank parkBank = new EmployeeBank("박찬호");
 		parkBank.saveMoney(100);
+		System.out.println();
 		
 		EmployeeBank leeBank = new EmployeeBank("이승엽");
 		leeBank.saveMoney(300);
+		System.out.println();
 		
 		leeBank.getBankInfo();
+		System.out.println();
+		
 		parkBank.getBankInfo();
+		System.out.println();
+
+		/*
+		leeBank.name = "박찬호"; // EmployeeBank클래스의 name변수가 public이므로
+								     변경 가능함
+		*/
 		
 		parkBank.spenMoney(400);
+		System.out.println();
 		
 		leeBank.getBankInfo();
-		parkBank.getBankInfo();
+		System.out.println();
 		
+		parkBank.getBankInfo();
+		System.out.println();
+		
+		Employee employee01 = new Employee("홍길동", 90);
+		employee01.getInfo();
+		
+		/*
+		employee01.score = 100; // Employee클래스의 score변수가 private이므로
+								       변경 불가능함
+		*/
+		
+		employee01.setScore(100);
+		employee01.getInfo();
 	}
 }
