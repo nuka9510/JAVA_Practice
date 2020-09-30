b9e04e7c126cf27985607e1b0f01c14a7fcc036a
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2020-09-30 16:51:53 +0900

    Add files via upload

---
+++ CodeUp/src/CodeUp/Codeup_1218.java
@@ -0,0 +1,43 @@
+﻿package CodeUp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Codeup_1218 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[3];
+		String result;
+		num[0] = sc.nextInt();
+		while(true) {
+			num[1] = sc.nextInt();
+			if(num[0]<=num[1]) {
+				num[2] = sc.nextInt();
+				if(num[1]<=num[2]) {
+					break;
+				}
+			}
+		}
+
+		Arrays.sort(num);
+
+		if(num[2]<num[0]+num[1]) {
+			if(num[0]==num[1]|num[0]==num[2]|num[1]==num[2]) {
+				result = "이등변삼각형";
+				if(num[0]==num[1]&num[0]==num[2]) {
+					result = "정삼각형";
+				}
+			}else if(Math.pow(num[0], 2)+Math.pow(num[1], 2)==Math.pow(num[2], 2)) { // Math class의 pow mathod는 제곱을 위한기능 이다.
+				result = "직각삼각형";
+			}else {
+				result = "삼각형";
+			}
+		}else {
+			result = "삼각형아님";
+		}
+		
+		System.out.println(result);
+
+		sc.close();
+	}
+}
