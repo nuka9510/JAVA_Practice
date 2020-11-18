996f6d79e9b8ffafc3060df7ce27a19050313496
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2020-11-18 23:52:56 +0900

    Add files via upload

---
+++ CodeUp/src/CodeUp/Codeup_1661.java
@@ -0,0 +1,40 @@
+﻿package CodeUp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Codeup_1661 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			String result = br.readLine();
+			result = result.replaceAll("\\s+", "");
+			result = result.replaceAll(",", " ");
+			result = result.replaceAll(";", " \n");
+			result += " ";
+			bw.write(result);
+			bw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+				if(bw != null) {
+					bw.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
---
+++ CodeUp/src/CodeUp/Codeup_1990.java
@@ -0,0 +1,41 @@
+﻿package CodeUp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Codeup_1990 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			String[] input = br.readLine().split("");
+			int result = 0;
+
+			for(String temp : input) {
+				result += Integer.parseInt(temp);
+			}
+
+			bw.write(((result%3)==0?1:0)+"");
+			bw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br!=null) {
+					br.close();	
+				}
+				if(bw!=null) {
+					bw.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
